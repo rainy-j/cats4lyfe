413fb232e7b95c96ee6825aa9bed7bd73fb85ea3
Ahmed mcgarry <ahmedmcgarry@Ahmeds-Mac-mini.local>
2021-10-27 17:18:20 +0100

    react router

--- src/App.js
+++ src/App.js
@@ -1,44 +1,72 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import faker from "faker";
-import './App.css';
-import Cat from "./components/Cat.js";
+import "./App.css";
+import Cat from "./components/Cat";
+import Home from "./components/Home";
+import CartImage from "./Images/icon-cart.svg";
 
 function App() {
-
-  const [cats, setCats] = useState ([]); 
-
+  const [cats, setCats] = useState([]);
 
   useEffect(() => {
     getCats();
   }, []);
 
-const getCats = async () => {
-    
-      let response = await fetch("https://api.thecatapi.com/v1/images/search?limit=4")
-      let data = await response.json(); 
-      for (let i = 0; i < data.length ; i++) {
-        const cat = data[i];
-        cat["name"] = faker.name.firstName()        
-        cat["age"] = faker.datatype.number(20)
-        cat["price"] = faker.commerce.price(50,500)
-        // console.log(data)
-        console.log(cat)
-      }
-      setCats(data)
-  }
+  const getCats = async () => {
+    let response = await fetch(
+      "https://api.thecatapi.com/v1/images/search?limit=4"
+    );
+    let data = await response.json();
+    for (let i = 0; i < data.length; i++) {
+      const cat = data[i];
+      cat["name"] = faker.name.firstName();
+      cat["age"] = faker.datatype.number(20);
+      cat["price"] = faker.commerce.price(50, 500);
+      // console.log(data)
+      console.log(cat);
+    }
+    setCats(data);
+  };
 
   return (
     <div className="App">
-<div>
-{cats.map((item, index) => {
-            return  (
-                <Cat item={item} index={index} key={index} />                                 
-                )
-            })
-    }
-</div>
+      <Router>
+        <div className="nav-container">
+          <div className="logo-container">
+            <h2>
+              CatsFor<span id="logo-span">Life</span>
+            </h2>
+          </div>
+
+          <div className="nav-bar">
+            <ul className="nav-list">
+              <li className="nav-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/cats">Cats</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="basket-container">
+            <img src={CartImage} alt="cart-icon" />
+          </div>
+        </div>
+        <Switch>
+          <Route path="/cats">
+          {cats.map((item, index) => {
+          return <Cat item={item} index={index} key={index} />;
+        })}
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+      <div>
+      </div>
     </div>
-    
   );
 }
 
