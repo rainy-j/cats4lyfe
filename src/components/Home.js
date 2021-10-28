9abb5889e786f3152f116b23ed28f2ce2e93e175
rainy-j <90918702+rainy-j@users.noreply.github.com>
2021-10-28 10:56:06 +0100

    updated home content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,10 @@
 const Home = () => {
-    return <div></div>
+    return <div className="homeContent">
+        <h1>CatsForLife</h1>
+        <h2>*not just for Christmas</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+        <button  Link to="/Cat.js">View Cats</button>
+    </div>
 }
 
 export default Home
