6a268f82ecbf482dc4605dd5cd695fe1bc1ba682
kasiasaif <kasia.saif@gmail.com>
2021-10-28 11:10:57 +0100

    cat css

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -5,13 +5,14 @@ const Cat = ({item}) => {
     
     return(
     <div className="row">
-        <div cat={item} className="line"/>          
+        <div cat={item} className="card">          
             <img src={item.url} alt="cat" width="200px" height="200px" className="images"></img>
-            <div>  
+            <div >  
                 <div className="text">Name: {item.name} </div>
                 <div className="text">Age: {item.age}</div>
-                <div className="text">Price: £{item.price}</div>
-                <button>Add</button>
+                <div className="price">£{item.price}</div>
+                <button>Add to basket</button>
+                </div>
             </div>          
     </div> 
     ) 
