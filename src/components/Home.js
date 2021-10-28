3ae217f53803c1bad1cdcceb50b8ff14156fbe9e
rainy-j <90918702+rainy-j@users.noreply.github.com>
2021-10-28 11:54:34 +0100

    updated cat button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom"
+
 const Home = () => {
+
+    // const handleOnClick = () => {
+    //    return <Link to="./Cat.js" <Link/>
+    // }
+
     return <div className="homeContent">
         <h1>CatsForLife</h1>
         <h2>*not just for Christmas</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-        <button  Link to="/Cat.js">View Cats</button>
+        <Link to="./Cat.js"> 
+            <button type="button">View Cats</button>
+        </Link>
     </div>
 }
 
